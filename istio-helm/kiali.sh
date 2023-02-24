a9b0706ebb18233fbde6b0896bb6d37723992e52
Paulo Vitor dos Santos <paulovitor8@gmail.com>
2023-02-24 02:17:33 -0300

    ajuste projeto

--- istio-helm/kiali.sh
+++
@@ -1,8 +0,0 @@
-helm install \
-  --namespace istio-system \
-  --set auth.strategy="anonymous" \
-  --repo https://kiali.org/helm-charts \
-  kiali-server \
-  kiali-server
-
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.17/samples/addons/prometheus.yaml
